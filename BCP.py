f9e00052550be38c365758f04a3e3d8be9cf4135
Daniel Bustamante <vtdanielf@gmail.com>
2024-09-15 22:02:19 -0500

    Se agrega lo de guardar los procesos en las colas

--- BCP.py
+++ BCP.py
@@ -9,12 +9,13 @@ def printLines():
 #Bloque de Control del Proceso
 class BCP:
     # Tupla de estados que pueden haber - La tupla es una estructura de datos inmutable
-    estados_procesos = ("listo","bloqueado","ejecucion","terminado")
+    estados_procesos = ("nuevo","listo","bloqueado","ejecucion","terminado")
 
     # Lsita donde se almacenaran todos los proceos
     procesos = []
 
-    #Cola que se usara para cada proceso
+    # Cola que se usara para cada proceso
+    cola_nuevo =[]
     cola_listo =[]
     cola_bloqueado =[]
     cola_ejecucion =[]
@@ -25,7 +26,7 @@ class BCP:
     recurso1 = []
     
     def creacion_proceos(self):
-        numero_procesos = int(input ("Ingrese el numero de procesos que desea crear "))
+        numero_procesos = int(input ("Ingrese el numero de procesos que desea crear: "))
         printLines()
         id_proceso = 00
 
@@ -38,18 +39,66 @@ class BCP:
             espacio = int(input("Ingrese el espacio requerido por el proceso: "))
             ejecuciones = int(input("Ingrese el numero de veces a ejecutar el proceos: "))
             numero_recursos = input("Ingrese los recursos que va a necesitar el proceso: ")
-            estado=input("Ingrese cual va a ser el estado inicial del proceso (listo,bloqueado,ejecucion,terminado): ")
+            
+            estado=input("Ingrese cual va a ser el estado inicial del proceso (nuevo,listo,bloqueado,ejecucion,terminado): ")
             while estado not in self.estados_procesos:
                 print("Ingrese un estado valido")
                 estado = input()
             printLines()
+            
+            # Creamos el proeceso y lo agregamos a la lista
             proceso = Procesos(id_proceso,espacio,estado,ejecuciones,numero_recursos)
+            # Agregamos el proceso a la lista de procesos
             self.procesos.append(proceso)
-        
+            
+            # Vamos a agregar el proceso a la lista del estado al cual pertenece
+            if estado == "nuevo":
+                self.cola_nuevo.append(proceso)
+            elif estado == "listo":
+                self.cola_listo.append(proceso)
+            elif estado == "bloqueado":
+                self.cola_bloqueado.append(proceso)
+            elif estado == "ejecucion":
+                self.cola_ejecucion.append(proceso)
+            else:
+                self.cola_terminado.append(proceso)
         
     
     def mostrar_procesos(self):
+        print("\tProcesos")
+        printLines()
+        
         for elemento in self.procesos:
             print(elemento.mostrarProceso())
+            printLines()
+        
+        print("\t\tColas de estados")
+        printLines()
+        
+        print("\tCola nuevos")
+        for elemento in self.cola_nuevo:
+            print("ID proceso:",elemento.get_id())
+        
+        printLines()
+        print("\tCola listos")    
+        for elemento in self.cola_listo:
+            print("ID proceso:",elemento.get_id())   
+
+        printLines()
+        print("\tCola bloqueados") 
+        for elemento in self.cola_bloqueado:
+            print("ID proceso:",elemento.get_id())
+        
+        printLines()
+        print("\tCola ejecucion") 
+        for elemento in self.cola_ejecucion:
+            print("ID proceso:",elemento.get_id())  
+
+        printLines()
+        print("\tCola terminado") 
+        for elemento in self.cola_terminado:
+            print("ID proceso:",elemento.get_id())  
+    
+    
         
        
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import BCP
+
+BCP.printLines()
+print("\tSimulador funcionamiento de un sistema operativo")
+BCP.printLines()
+
 bcp = BCP.BCP()
+
 bcp.creacion_proceos()
 bcp.mostrar_procesos()
