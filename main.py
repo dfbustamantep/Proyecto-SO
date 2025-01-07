51cc9ae6731d84a544913a7907a2a68009ad7320
Daniel Bustamante <vtdanielf@gmail.com>
2025-01-06 22:32:49 -0500

    Creacion menu con navbar

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from BCP import BCP
 from memoria import Memoria
+
+from flask import request,make_response,redirect,render_template,session,url_for,flash
+from app import create_app
+
+app = create_app()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    #desde cualquier direccion
+    app.run(host='0.0.0.0',port=81,debug=True)
+
+
+'''
+
 def printLines():
     print("----------------------------------------------------------------------")
 
@@ -20,3 +37,4 @@ print()
 if(ejecutar=='s' or ejecutar=='S'):
     print("Ejecucion")
     bcp.ejecutar()
+    '''
